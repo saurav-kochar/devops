54481a34bacc106a625778c71b65bbe6133bbfd5
Saurav Kochar <skochar.contractor@libertyglobal.com>
2025-07-18 14:19:04 +0200

    add gitignore

--- azure/azure_storage/main.tf
+++ azure/azure_storage/main.tf
@@ -12,15 +12,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "tf_rg1_sk" {
+resource "azurerm_resource_group" "tf_rg1" {
   name     = "tf_rg1_sk"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "new_storage_account" {
   name                     = "tfsksa1"
-  resource_group_name      = azurerm_resource_group.tf_rg1_sk.name
-  location                 = azurerm_resource_group.tf_rg1_sk.location
+  resource_group_name      = azurerm_resource_group.tf_rg1.name
+  location                 = azurerm_resource_group.tf_rg1.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
